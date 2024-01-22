a02654dce4fe366102901687c33a7fefa39c0024
pimpareshubhams <157403239+pimpareshubhams@users.noreply.github.com>
2024-01-22 23:19:12 +0530

    Update Home.js

--- final-fe/src/pages/Home.js
+++ final-fe/src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-    <div className='homepage'>
+    <div className='homepage card'>
       <p className='head'>Mindful Gurukul's</p>
       <p className='tagline'>Full Stack Developer Task</p>
     </div>
